6b03f294a165bba03e9216b318bdd78005dea7fa
Ahmed Waleed <62625165+BoiOG@users.noreply.github.com>
2025-04-02 21:57:17 -0600

    updated all cogs in setup_hook

--- main.py
+++ main.py
@@ -20,10 +20,18 @@ bot = commands.Bot(command_prefix="?", intents=intents)
 # Load all command modules (cogs)
 @bot.event
 async def setup_hook():
-    await bot.load_extension("commands.stock")
+    await bot.load_extension("commands.claim")
     await bot.load_extension("commands.keygen")
+    await bot.load_extension("commands.load_pull")
     await bot.load_extension("commands.logs")
     await bot.load_extension("commands.misc")
+    await bot.load_extension("commands.notes")
+    await bot.load_extension("commands.order")
+    await bot.load_extension("commands.redeemcode")
+    await bot.load_extension("commands.replace")
+    await bot.load_extension("commands.reward")
+    await bot.load_extension("commands.stock")
+    await bot.load_extension("commands.util")
 
 # Basic startup log
 @bot.event
